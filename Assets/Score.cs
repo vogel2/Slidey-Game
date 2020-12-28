07452bec90cc6fcb7af05b4aa5d5cb7dac0fe9e4
vogel2 <48958459+vogel2@users.noreply.github.com>
2020-12-28 11:27:12 +0200

    7 - Game Score Implementation

--- Assets/Score.cs
+++ Assets/Score.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour
 {
+    public Transform Player;
+    public Text ScoreText;
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        ScoreText.text = (Player.position.z / 200).ToString("0");
     }
 }
+
